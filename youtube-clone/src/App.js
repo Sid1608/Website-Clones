be69c22c9cf192bc3f61e3f1f681dfefb04b3747
Sid1608 <akarsiddharth@gmail.com>
2022-02-11 23:21:49 +0530

    Added youtube-clone files

---
+++ youtube-clone/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from './Header';
+import './App.css';
+
+function App() {
+  return (
+
+    //BEM class naming convention
+    <div className="App">
+      <Header/>
+      {/* Header */}
+      {/* Sidebar */}
+      {/* RecommendeVideo */}
+    </div>
+  );
+}
+
+export default App;
---
+++ youtube-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCP6gkRMbvpq2iktmKVfDu_eISNgYoJjdw",
+    authDomain: "clone-10d49.firebaseapp.com",
+    projectId: "clone-10d49",
+    storageBucket: "clone-10d49.appspot.com",
+    messagingSenderId: "830519116557",
+    appId: "1:830519116557:web:6b40ec7b26e5eb63512644",
+    measurementId: "G-2G6TE4M3FW"
+  };
