63746d5f936c065db99b5438747965191dd42d45
Sid1608 <akarsiddharth@gmail.com>
2022-02-15 16:18:18 +0530

    added Input design in Feed section

--- linkedin-clone/src/components/Feed/Feed.js
+++ linkedin-clone/src/components/Feed/Feed.js
@@ -1,6 +1,11 @@
 import CreateIcon from '@mui/icons-material/Create';
+import ImageIcon from '@mui/icons-material/Image';
+import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
+import EventNoteIcon from '@mui/icons-material/EventNote';
+import CalendarViewDayIcon from '@mui/icons-material/CalendarViewDay';
 import React from 'react'
 import './Feed.css'
+import InputOption from './InputOption';
 const Feed = () => {
     return (
         <div className="feed">
@@ -13,7 +18,10 @@ const Feed = () => {
                     </form>
                 </div>
                 <div className="feed__inputOptions">
-                    
+                          <InputOption Icon={ImageIcon} title="Photo" color="#70B5F9"/>
+                          <InputOption Icon={SubscriptionsIcon} title="Video" color="#E7A33E"/>
+                          <InputOption Icon={EventNoteIcon} title="Event" color="#C0CBCD"/>
+                          <InputOption Icon={CalendarViewDayIcon} title="Write article" color="#7FC15E"/>
                 </div>
             </div>
         </div>
