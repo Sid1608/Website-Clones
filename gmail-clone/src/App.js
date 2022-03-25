426ff89dd166f7b70726b9d4fbe379ee0f32516e
sid1608 <akarsiddharth@gmail.com>
2022-03-25 17:05:54 +0530

    added emailList setting, sections(primary, social, promotions)

--- gmail-clone/src/App.js
+++ gmail-clone/src/App.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import './App.css';
 import Header from './components/header/Header';
-
+import Sidebar from './components/sidebar/Sidebar';
+import Mail from './components/mail/Mail';
+import EmailList from './components/emailList/EmailList'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+}from 'react-router-dom';
 function App() {
   return (
-    <div className="app">
-      <Header/>
-      <h1>Lets build gmail</h1>
-    </div>
+    <Router>
+      <div className="app">
+        <Header/>
+        <div className="app__body">
+          <Sidebar/>
+          <Routes>
+            <Route path="/mail" element={<Mail/>}/>
+            <Route path="/" element={ <EmailList/>}/>
+
+          </Routes>
+        </div>
+        {/* <Sidebar/> */}
+      </div>
+
+    </Router>
+    
   );
 }
 
