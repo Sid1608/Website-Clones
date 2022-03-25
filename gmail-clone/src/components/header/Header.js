e120bce99cf6a52f363f1fa684b73a77f3edec90
sid1608 <akarsiddharth@gmail.com>
2022-03-25 12:27:08 +0530

    header section completed

--- gmail-clone/src/components/header/Header.js
+++ gmail-clone/src/components/header/Header.js
@@ -1,16 +1,33 @@
 import React from 'react'
 import './Header.css';
-const Header = () => {
+import MenuIcon from '@mui/icons-material/Menu';
+import {IconButton,Avatar} from  "@mui/material";
+import SearchIcon from '@mui/icons-material/Search'
+import { ArrowDropDown } from '@mui/icons-material';
+import AppsIcon from '@mui/icons-material/Apps';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+const Header = () => { 
     return (
         <div className="header">
             <div className="header__left">
-
+                <IconButton>
+                    <MenuIcon/>
+                </IconButton>
+                <img src="https://cdn5-ss14.sharpschool.com/UserFiles/Servers/Server_11798593/Image/Students%20page/Gmail-logo-color.jpg" alt=""/>
             </div>
             <div className="header__middle">
-
+                <SearchIcon />
+                <input placeholder="Search mail" type="text"/>
+                <ArrowDropDown className="header__inputCaret"/>
             </div>
             <div className="header__right">
-
+                <IconButton>
+                    <AppsIcon/>
+                </IconButton>
+                <IconButton>
+                    <NotificationsIcon/>
+                </IconButton>
+                <Avatar/>
             </div>
         </div>
     )
